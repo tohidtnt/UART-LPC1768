c38cd51385ac7abffe145ed650c60dafff66f681
tohidtnt <tohid.gadiri@gmail.com>
2018-08-13 23:05:45 +0430

    complate an usart communication

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include "LPC17xx.h"
-
+#include "Serial.h"
+#include "stdio.h"
 
 int main()
-{
-
-
-
-
+{	
+	while (1)
+	{
+		SER_Init();
+		char text1[20], text2[20];
+		uint16_t i_val;
+		SER_Init();
+		puts("\n\rEnter Your Text1:");
+		scanf("%s", text1);
+		printf("\n\r Text1 IS:%s\n", text1);
+	}
 }
