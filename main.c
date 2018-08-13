fe3d003c15084e666d079730e5fd052bcbd143db
tohidtnt <tohid.gadiri@gmail.com>
2018-08-14 01:26:36 +0430

    v2 cod is simple struct test by worker 1

--- main.c
+++ main.c
@@ -2,16 +2,39 @@
 #include "Serial.h"
 #include "stdio.h"
 
+
+
+ struct student
+{
+	int id;
+	char *firstName;
+	char *lastName;
+	float gpa;
+
+} ;
+
+
+void displayStudentInformation(struct student stu)
+{
+	printf("Student information is :\n\r");
+	printf("                    id : %d \n\r", stu.id);
+	printf("            First Name : %s \n\r", stu.firstName);
+	printf("             Last Name : %s \n\r", stu.lastName);
+	printf("                GPA is : %.2f \n\r", stu.gpa);
+  printf("---------------------------------------\n\r");
+}
 int main()
-{	
-	while (1)
-	{
-		SER_Init();
-		char text1[20], text2[20];
-		uint16_t i_val;
-		SER_Init();
-		puts("\n\rEnter Your Text1:");
-		scanf("%s", text1);
-		printf("\n\r Text1 IS:%s\n", text1);
-	}
+{
+	SER_Init();
+	struct student stu1;
+	stu1.firstName = "ALI";
+	stu1.lastName = "ALAVI";
+	stu1.id = 101;
+	stu1.gpa = 18;
+	struct student stu2 = { 102,"REZA","RAZAVI",18 };
+
+	displayStudentInformation(stu1);
+    displayStudentInformation(stu2);
+
+
 }
